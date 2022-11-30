705cd99475f0ccf0ec27c02c011bc2ea002b121f
Onur Can <hzregista@gmail.com>
2022-11-30 18:51:22 +0300

    kodlama.io Java Spring Boot 1. Ders

---
+++ src/main/java/kodlama/io/rentACar/dataAccess/abstracts/BrandRepository.java
@@ -0,0 +1,10 @@
+package kodlama.io.rentACar.dataAccess.abstracts;
+
+import kodlama.io.rentACar.entities.concretes.Brand;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface BrandRepository {
+     List<Brand> getAll();
+}
